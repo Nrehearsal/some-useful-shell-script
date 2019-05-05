c01b6a284c7d8fb01b4a6e4b6cf15de6f4ae27cc
root <root@DESKTOP-E92RQ9C.localdomain>
2019-05-05 18:52:23 +0800

    自动执行ssh-copy-id, 并处理相关异常情况, 自动ssh登录并执行命令, fix bug

--- network_test.sh
+++ network_test.sh
@@ -1,42 +1,64 @@
 #!/bin/bash
 
 target_list=
-server_list=
+host=
 username=
 password=
 
 #automatically execute ssh-copy-id
 ssh_copy_id() {
-echo 'set timeout 3
-spawn ssh-copy-id -o connecttimeout=2 -i '$HOME'/.ssh/id_rsa.pub '$1'@'$2'
+echo 'set timeout 5
+spawn ssh-copy-id -i '$HOME'/.ssh/id_rsa.pub '$username'@'$host'
 expect {
 	#first connect, no public key in ~/.ssh/known_hosts
 	"(yes/no)?" {
 		send "yes\r"
-		expect "s password:"
-			send "'$3'\r"
+		puts "new client"
+		expect "*password:" {
+			send "'$password'\r"
+			puts "password send"
+			expect "please try again" {
+				puts "password is incorrect"
+				exit 4
+			}
+		}
 	}
 	#already has public key in ~/.ssh/known_hosts
-	"s password:" {
-		send "'$3'\r"
+	"*password:" {
+		send "'$password'\r"
+		puts "old client"
+		puts "password send"
+		expect "please try again" {
+			puts "password is incorrect"
+			exit 4
+		}
+
 	}
 	"All keys were skipped because they already exist on the remote system"
 	{
+		puts "ssh key already on remote"
+		exit 0
 	}
 	"Resource temporarily unavailable" {
+		puts "connected failed\r"
 		exit 4
 	}
 	"Connection timed out" {
+		puts "connected timeout"
 		exit 4
 	}
 	timeout {
+		puts "expect timeout"
 		exit 4
 	}
+	eof
+}' | expect
 }
-expect of' | /usr/bin/expect >> /dev/null
-}
-            
+
+           
 main (){
+	echo "run env: [$host:$username:$password]"
+
 	`which expect >> /dev/null`
 	if [ $? -ne 0 ]
 	then
@@ -51,57 +73,46 @@ main (){
 		exit -1
 	fi
 
-	if  !(test -s $server_list)
-	then
-		echo "$server_list does not exist or it is empty"
-		exit -1
-	fi
-
 	if  !(test -s $target_list)
 	then
-		echo "$server_list does not exist or it is empty"
+		echo "$target_list does not exist or it is empty"
 		exit -1
 	fi
 
+		
+	#ssh-copy-id $host
+	ssh_copy_id
+	if [ $? -ne 0 ]
+	then
+		echo "[error]ssh_copy_id to [$host] failed."
+		exit -1
+	else
+		echo "[success]ssh key deploy on [$host] successed."
+	fi
 
-	for host in `cat $server_list`
+	echo "------------------------------------------"
+	echo "[log]start to test connectivity on [$host]"
+
+	for target in `cat $target_list`
 	do
-		#ssh-copy-id $host
-		ssh_copy_id $username $host $password
+		target="${target/:/ }"
+		cmd='ssh -o connecttimeout=2 '$username'@'$host' nc -z -w 2 '$target''
+		#echo cmd=[$cmd]
+		`$cmd`
 		if [ $? -ne 0 ]
 		then
-			echo "[error]ssh_copy_id to [$host] failed."
-			echo "[next]go to next server."
-			continue
+			echo "[error][$host] connect to [$target] failed."
+			#echo "------------------------------------------"
 		else
-			echo "[success]copy ssh key to [$host] successed."
+			echo "[success][$host] connect to [$target] success."
+			#echo "------------------------------------------"
 		fi
-
-		echo "------------------------------------------"
-		echo "[log]start to test connectivity on [$host]"
-		for target in `cat $target_list`
-		do
-			target="${target/":"/" "}"
-			cmd='ssh -o connecttimeout=2 '$username'@'$host' nc -z -w 2 '$target''
-			#echo cmd=[$cmd]
-			`$cmd`
-			if [ $? -ne 0 ]
-			then
-				echo "[error][$host] connect to [$target] failed."
-				#echo "------------------------------------------"
-			else
-				echo "[success][$host] connect to [$target] success."
-				#echo "------------------------------------------"
-			fi
-		done
-		echo "------------------------------------------"
-		echo "------------------------------------------"
 	done
 }
 
 usage() 
 {
-	echo "Usage: $0 -t[test target file] -s[server list file] -u[username for each server] -p[password for server user]" 
+	echo "Usage: $0 -t[test target file] -h[host for check] -u[username] -p[password]" 
 	exit -1
 }
 
@@ -109,14 +120,14 @@ usage()
 #start from here
 [ $# -eq 0 ] && usage
 
-while getopts :t:s:u:p: OPTION
+while getopts :t:h:u:p: OPTION
 do
 	case $OPTION in
 		t)
 			target_list=$OPTARG
 			;;
-		s)
-			server_list=$OPTARG
+		h)
+			host=$OPTARG
 			;;
 		u)
 			username=$OPTARG
